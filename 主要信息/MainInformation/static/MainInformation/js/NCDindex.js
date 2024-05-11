e14c14876729b740b28eda5691c3a8fc1a21ef60
BoringMax <135253506+StatisticsBorui@users.noreply.github.com>
2024-05-12 00:38:59 +0800

    Add files via upload

---
+++ 主要信息/MainInformation/static/MainInformation/js/NCDindex.js
@@ -0,0 +1,762 @@
+var symptomName = last_month_day();
+
+$(function(){
+
+
+  init();
+
+})
+function init(){
+
+    //各医院门诊人次
+  var histogramChart1 = echarts.init(document.getElementById('histogramChart1'));
+  histogramChart1.setOption({
+
+     grid: {
+         top: '20%',
+         left: '32%'
+     },
+     xAxis: {
+         show: false
+     },
+     yAxis: [{
+         show: true,
+         data:  ['厦门第一医院','厦门中山医院','厦门中医院','厦门第五医院',],
+         inverse: true,
+         axisLine: {
+             show: false
+         },
+         splitLine: {
+             show: false
+         },
+         axisTick: {
+             show: false
+         },
+         axisLabel: {
+             color: '#fff',
+             formatter: (value, index) => {
+                 return [
+
+                     `{lg|${index+1}}  ` + '{title|' + value + '} '
+                 ].join('\n')
+             },
+             rich: {
+                 lg: {
+                     backgroundColor: '#339911',
+                     color: '#fff',
+                     borderRadius: 15,
+                     // padding: 5,
+                     align: 'center',
+                     width: 15,
+                     height: 15
+                 },
+             }
+         },
+
+
+     }, {
+         show: true,
+         inverse: true,
+         data: [4000, 3000, 2000, 1000],
+         axisLabel: {
+             textStyle: {
+                 fontSize: 12,
+                 color: '#fff',
+             },
+         },
+         axisLine: {
+             show: false
+         },
+         splitLine: {
+             show: false
+         },
+         axisTick: {
+             show: false
+         },
+
+     }],
+     series: [{
+         name: '条',
+         type: 'bar',
+         yAxisIndex: 0,
+         data: [40, 30, 20, 10],
+         barWidth: 10,
+         itemStyle: {
+             normal: {
+                 barBorderRadius: 20,
+                 color: function(params) {
+                     var num = myColor.length;
+                     return myColor[params.dataIndex % num]
+                 },
+             }
+         },
+         label: {
+             normal: {
+                 show: true,
+                 position: 'inside',
+                 formatter: '{c}%'
+             }
+         },
+     }, {
+         name: '框',
+         type: 'bar',
+         yAxisIndex: 1,
+         barGap: '-100%',
+         data: [100, 100, 100, 100],
+         barWidth: 15,
+         itemStyle: {
+             normal: {
+                 color: 'none',
+                 borderColor: '#00c1de',
+                 borderWidth: 3,
+                 barBorderRadius: 15,
+             }
+         }
+     }, ]
+  })
+
+    
+
+
+
+  //地图
+  var mapChart = echarts.init(document.getElementById('mapChart'));
+  mapChart.setOption({
+      bmap: {
+          center: [118.096435,24.485408],
+          zoom: 12,
+          roam: true,
+
+      },
+      tooltip : {
+          trigger: 'item',
+          formatter:function(params, ticket, callback){
+              return params.value[2]
+          }
+      },
+      series: [{
+          type: 'scatter',
+          coordinateSystem: 'bmap',
+          data: [
+            [118.096435, 24.485408, '厦门市'] ,
+            [118.094564, 24.457358, '厦门第一医院'] ,
+            [118.104103, 24.477761, '厦门中山医院'],
+            [118.14748, 24.506295, '厦门中医院'],
+            [118.254841, 24.665349, '厦门第五医院'],
+           ]
+      }]
+  });
+  mapChart.on('click', function (params) {
+    console.log(params.value[2])
+    // $("#hospitalName").html(params.value[2]);
+    // if(params.value[2] == '厦门市'){
+    //   $("#Data").html(100);
+    //   $("#Outpatient").html(20000);
+    //   $("#Hospitalization").html(3000);
+    // }
+    // if(params.value[2] == '厦门第一医院'){
+    //   $("#Data").html(40);
+    //   $("#Outpatient").html(8000);
+    //   $("#Hospitalization").html(1200);
+    // }
+    // if(params.value[2] == '厦门中山医院'){
+    //   $("#Data").html(30);
+    //   $("#Outpatient").html(6000);
+    //   $("#Hospitalization").html(900);
+    // }
+    // if(params.value[2] == '厦门中医院'){
+    //   $("#Data").html(20);
+    //   $("#Outpatient").html(4000);
+    //   $("#Hospitalization").html(600);
+    // }
+    // if(params.value[2] == '厦门第五医院'){
+    //   $("#Data").html(10);
+    //   $("#Outpatient").html(2000);
+    //   $("#Hospitalization").html(300);
+    // }
+  });
+
+  var bmap = mapChart.getModel().getComponent('bmap').getBMap()
+  bmap.addControl(new BMap.MapTypeControl({mapTypes: [BMAP_NORMAL_MAP,BMAP_SATELLITE_MAP ]}));
+  bmap.setMapStyle({style:'midnight'})
+
+
+  var pieChart = echarts.init(document.getElementById('pieChart'));
+  pieChart.setOption({
+
+    color:["#87cefa","#ff7f50","#32cd32","#da70d6","#FD6C88"],
+
+    legend: {
+        y : '260',
+        x : 'center',
+        textStyle : {
+            color : '#ffffff',
+
+        },
+         data : ['高血压','糖尿病','脑卒中','慢阻肺','慢性肾病'],
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a}<br/>{b}<br/>{c}G ({d}%)"
+    },
+    calculable : false,
+    series : [
+        {
+            name:'采集数据量',
+            type:'pie',
+            radius : ['40%', '70%'],
+            center : ['50%', '45%'],
+            itemStyle : {
+                normal : {
+                    label : {
+                        show : false
+                    },
+                    labelLine : {
+                        show : false
+                    }
+                },
+                emphasis : {
+                    label : {
+                        show : true,
+                        position : 'center',
+                        textStyle : {
+                            fontSize : '20',
+                            fontWeight : 'bold'
+                        }
+                    }
+                }
+            },
+            data:[
+                {value:335, name:'高血压'},
+                {value:310, name:'糖尿病'},
+                {value:234, name:'脑卒中'},
+                {value:135, name:'慢阻肺'},
+				{value:235, name:'慢性肾病'}
+            ]
+        }
+    ]
+    });
+
+
+    var lineChart = echarts.init(document.getElementById('lineChart'));
+    lineChart.setOption({
+
+      color:["#87cefa","#ff7f50","#32cd32","#da70d6",],
+      legend: {
+          y : '260',
+          x : 'center',
+          textStyle : {
+              color : '#ffffff',
+
+          },
+           data : ['厦门第一医院','厦门中山医院','厦门中医院','厦门第五医院',],
+      },
+      calculable : false,
+      tooltip : {
+          trigger: 'item',
+          formatter: "{a}<br/>{b}<br/>{c}条"
+      },
+      yAxis: [
+            {
+                type: 'value',
+                axisLine : {onZero: false},
+                axisLine:{
+                    lineStyle:{
+                        color: '#034c6a'
+                    },
+                },
+
+                axisLabel: {
+                    textStyle: {
+                        color: '#fff'
+                    },
+                    formatter: function (value) {
+                        return value + "k条"
+                    },
+                },
+                splitLine:{
+                    lineStyle:{
+                        width:0,
+                        type:'solid'
+                    }
+                }
+            }
+        ],
+        xAxis: [
+            {
+                type: 'category',
+                data : ['8:00','10:00','12:00','14:00','16:00','18:00','20:00','22:00'],
+                axisLine:{
+                    lineStyle:{
+                        color: '#034c6a'
+                    },
+                },
+                splitLine: {
+                    "show": false
+                },
+                axisLabel: {
+                    textStyle: {
+                        color: '#fff'
+                    },
+                    formatter: function (value) {
+                        return value + ""
+                    },
+                },
+                splitLine:{
+                    lineStyle:{
+                        width:0,
+                        type:'solid'
+                    }
+                },
+            }
+        ],
+        grid:{
+                left: '5%',
+                right: '5%',
+                bottom: '20%',
+                containLabel: true
+        },
+        series : [
+          {
+              name:'厦门第一医院',
+              type:'line',
+              smooth:true,
+              itemStyle: {
+                  normal: {
+                      lineStyle: {
+                          shadowColor : 'rgba(0,0,0,0.4)'
+                      }
+                  }
+              },
+              data:[15, 0, 20, 45, 22.1, 25, 70, 55, 76]
+          },
+          {
+              name:'厦门中山医院',
+              type:'line',
+              smooth:true,
+              itemStyle: {
+                  normal: {
+                      lineStyle: {
+                          shadowColor : 'rgba(0,0,0,0.4)'
+                      }
+                  }
+              },
+              data:[25, 10, 30, 55, 32.1, 35, 80, 65, 76]
+          },
+          {
+              name:'厦门中医院',
+              type:'line',
+              smooth:true,
+              itemStyle: {
+                  normal: {
+                      lineStyle: {
+                          shadowColor : 'rgba(0,0,0,0.4)'
+                      }
+                  }
+              },
+              data:[35, 20, 40, 65, 42.1, 45, 90, 75, 96]
+          },
+          {
+              name:'厦门第五医院',
+              type:'line',
+              smooth:true,
+              itemStyle: {
+                  normal: {
+                      lineStyle: {
+                          shadowColor : 'rgba(0,0,0,0.4)'
+                      }
+                  }
+              },
+              data:[45, 30, 50, 75, 52.1, 55, 100, 85, 106]
+          }
+      ]
+    });
+
+    var histogramChart = echarts.init(document.getElementById('histogramChart'));
+    histogramChart.setOption({
+
+      color:["#87cefa","#ff7f50","#32cd32","#da70d6",],
+      legend: {
+          y : '250',
+          x : 'center',
+          data:['厦门第一医院', '厦门中山医院','厦门中医院','厦门第五医院'],
+          textStyle : {
+              color : '#ffffff',
+
+          }
+      },
+
+      calculable :false,
+
+
+      grid:{
+              left: '5%',
+              right: '5%',
+              bottom: '20%',
+              containLabel: true
+      },
+
+      tooltip : {
+          trigger: 'axis',
+          axisPointer : {
+              type : 'shadow'
+          }
+      },
+
+      xAxis : [
+          {
+              type : 'value',
+              axisLabel: {
+                  show: true,
+                  textStyle: {
+                      color: '#fff'
+                  }
+              },
+              splitLine:{
+                  lineStyle:{
+                      color:['#f2f2f2'],
+                      width:0,
+                      type:'solid'
+                  }
+              }
+
+          }
+      ],
+
+      yAxis : [
+          {
+              type : 'category',
+              data:['门诊人数(人)', '住院人次(人)','人均费用(元)'],
+              axisLabel: {
+                  show: true,
+                  textStyle: {
+                      color: '#fff'
+                  }
+              },
+              splitLine:{
+                  lineStyle:{
+                      width:0,
+                      type:'solid'
+                  }
+              }
+          }
+      ],
+
+      series : [
+          {
+              name:'厦门第一医院',
+              type:'bar',
+              stack: '总量',
+              itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+              data:[320, 302, 301]
+          },
+          {
+              name:'厦门中山医院',
+              type:'bar',
+              stack: '总量',
+              itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+              data:[120, 132, 101]
+          },
+          {
+              name:'厦门中医院',
+              type:'bar',
+              stack: '总量',
+              itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+              data:[220, 182, 191]
+          },
+          {
+              name:'厦门第五医院',
+              type:'bar',
+              stack: '总量',
+              itemStyle : { normal: {label : {show: true, position: 'insideRight'}}},
+              data:[150, 212, 201]
+          }
+
+      ]
+   });
+
+   var lineChart2 = echarts.init(document.getElementById('lineChart2'));
+   lineChart2.setOption({
+
+     color:["#87cefa","#ff7f50","#32cd32","#da70d6",],
+     legend: {
+         y : '260',
+         x : 'center',
+         textStyle : {
+             color : '#ffffff',
+
+         },
+          data : ['厦门第一医院','厦门中山医院','厦门中医院','厦门第五医院',],
+     },
+     calculable : false,
+     tooltip : {
+         trigger: 'item',
+         formatter: "{a}<br/>{b}<br/>{c}条"
+     },
+     yAxis: [
+           {
+               type: 'value',
+               axisLine : {onZero: false},
+               axisLine:{
+                   lineStyle:{
+                       color: '#034c6a'
+                   },
+               },
+
+               axisLabel: {
+                   textStyle: {
+                       color: '#fff'
+                   },
+                   formatter: function (value) {
+                       return value + "条"
+                   },
+               },
+               splitLine:{
+                   lineStyle:{
+                       width:0,
+                       type:'solid'
+                   }
+               }
+           }
+       ],
+       xAxis: [
+           {
+               type: 'category',
+               data : ['8:00','10:00','12:00','14:00','16:00','18:00'],
+               axisLine:{
+                   lineStyle:{
+                       color: '#034c6a'
+                   },
+               },
+               splitLine: {
+                   "show": false
+               },
+               axisLabel: {
+                   textStyle: {
+                       color: '#fff'
+                   },
+                   formatter: function (value) {
+                       return value + ""
+                   },
+               },
+               splitLine:{
+                   lineStyle:{
+                       width:0,
+                       type:'solid'
+                   }
+               },
+           }
+       ],
+       grid:{
+               left: '5%',
+               right: '5%',
+               bottom: '20%',
+               containLabel: true
+       },
+       series : [
+         {
+             name:'厦门第一医院',
+             type:'line',
+             smooth:true,
+             itemStyle: {
+                 normal: {
+                     lineStyle: {
+                         shadowColor : 'rgba(0,0,0,0.4)'
+                     }
+                 }
+             },
+             data:[15, 0, 20, 45, 22.1, 25,].reverse()
+         },
+         {
+             name:'厦门中山医院',
+             type:'line',
+             smooth:true,
+             itemStyle: {
+                 normal: {
+                     lineStyle: {
+                         shadowColor : 'rgba(0,0,0,0.4)'
+                     }
+                 }
+             },
+             data:[25, 10, 30, 55, 32.1, 35, ].reverse()
+         },
+         {
+             name:'厦门中医院',
+             type:'line',
+             smooth:true,
+             itemStyle: {
+                 normal: {
+                     lineStyle: {
+                         shadowColor : 'rgba(0,0,0,0.4)'
+                     }
+                 }
+             },
+             data:[35, 20, 40, 65, 42.1, 45, ].reverse()
+         },
+         {
+             name:'厦门第五医院',
+             type:'line',
+             smooth:true,
+             itemStyle: {
+                 normal: {
+                     lineStyle: {
+                         shadowColor : 'rgba(0,0,0,0.4)'
+                     }
+                 }
+             },
+             data:[45, 30, 50, 75, 52.1, 55, 6].reverse()
+         }
+     ]
+   });
+    
+
+
+
+    var myColor = ['#1089E7', '#F57474', '#56D0E3', '#F8B448', '#8B78F6'];
+    //水文气象
+  var histogramChart0 = echarts.init(document.getElementById('histogramChart0'));
+  histogramChart0.setOption({
+
+     grid: {
+         top: '20%',
+         left: '32%'
+     },
+     xAxis: {
+         show: false
+     },
+     yAxis: [{
+         show: true,
+         data:  ['电池电压(V)','盐度(‰)','溶解度(mg/L)','浊度(NTU)','pH','水温(°C)'],
+         inverse: true,
+         axisLine: {
+             show: false
+         },
+         splitLine: {
+             show: false
+         },
+         axisTick: {
+             show: false
+         },
+         axisLabel: {
+             color: '#fff',
+             formatter: (value, index) => {
+                 return [
+
+                     `{lg|${index+1}}  ` + '{title|' + value + '} '
+                 ].join('\n')
+             },
+             rich: {
+                 lg: {
+                     backgroundColor: '#339911',
+                     color: '#fff',
+                     borderRadius: 15,
+                     // padding: 5,
+                     align: 'center',
+                     width: 15,
+                     height: 15
+                 },
+             }
+         },
+
+
+     }, {
+         show: true,
+         inverse: true,
+         data: [4000, 3000, 2000, 1000,1000,1000],
+         axisLabel: {
+             textStyle: {
+                 fontSize: 12,
+                 color: '#fff',
+             },
+         },
+         axisLine: {
+             show: false
+         },
+         splitLine: {
+             show: false
+         },
+         axisTick: {
+             show: false
+         },
+
+     }],
+     series: [{
+         name: '条',
+         type: 'bar',
+         yAxisIndex: 0,
+         data: [40, 30, 20, 10,10,10],
+         barWidth: 10,
+         itemStyle: {
+             normal: {
+                 barBorderRadius: 20,
+                 color: function(params) {
+                     var num = myColor.length;
+                     return myColor[params.dataIndex % num]
+                 },
+             }
+         },
+         label: {
+             normal: {
+                 show: true,
+                 position: 'inside',
+                 formatter: '{c}%'
+             }
+         },
+     }, {
+         name: '框',
+         type: 'bar',
+         yAxisIndex: 1,
+         barGap: '-100%',
+         data: [100, 100, 100, 100,100,100],
+         barWidth: 15,
+         itemStyle: {
+             normal: {
+                 color: 'none',
+                 borderColor: '#00c1de',
+                 borderWidth: 3,
+                 barBorderRadius: 15,
+             }
+         }
+     }, ]
+  });
+
+
+}
+
+
+//额外添加
+// 在页面加载完成后执行初始设置
+window.addEventListener('DOMContentLoaded', function() {
+  var videos = document.getElementsByTagName('video');
+  for (var i = 0; i < videos.length; i++) {
+    if (i === 0) {
+      videos[i].style.display = 'block'; // 显示第一个视频
+      videos[i].play();
+    } else {
+      videos[i].style.display = 'none'; // 隐藏其他视频
+    }
+  }
+});
+
+function changeVideo(videoIndex) {
+  var videos = document.getElementsByTagName('video');
+  var buttons = document.getElementsByTagName('button');
+
+  for (var i = 0; i < videos.length; i++) {
+    videos[i].pause();
+    videos[i].currentTime = 0;
+    videos[i].style.display = 'none';
+    buttons[i].classList.remove('active');
+  }
+
+  var targetVideo = videos[videoIndex];
+  var targetButton = buttons[videoIndex];
+  targetVideo.style.display = 'block';
+  targetVideo.play();
+  targetButton.classList.add('active');
+}
+
+
+
+
+
