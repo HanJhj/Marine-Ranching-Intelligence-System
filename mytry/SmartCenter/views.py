6453808cf002c360862eace5e901effd836128d2
HanJhj <2331772430@qq.com>
2024-05-11 13:53:10 +0800

    补全

---
+++ mytry/SmartCenter/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def SmartCenter(request):
+    context = {'title': 'My Page Title'}  # 数据字典
+    # return render(request, '/templates/trend.html', context)  # 使用模板
+    return render(request, 'SmartCenter/trend.html', context)
