d8c3a4b416af702f52fed2a887088bacd1ac2d77
huanhua2329 <2630488187@qq.com>
2024-05-12 16:17:06 +0800

    水下系统实现跳转

--- mytry/UnderWater/urls.py
+++ mytry/UnderWater/urls.py
@@ -4,5 +4,6 @@ from . import views  # 假设你的视图函数在 views.py 文件中
   
 urlpatterns = [  
     path('', views.UnderWater, name='UnderWater'),  # 空路径（即/learn/）将会调用learn应用的index视图 
+    path('UnderWater_user/', views.UnderWater_user, name='UnderWater_user'),
     # ...  
 ]
--- mytry/UnderWater/views.py
+++ mytry/UnderWater/views.py
@@ -6,3 +6,8 @@ def UnderWater(request):
     context = {'title': 'My Page Title'}  # 数据字典
     # return render(request, '/templates/trend.html', context)  # 使用模板
     return render(request, 'UnderWater/UnderWater.html', context)
+
+def UnderWater_user(request):
+    context = {'title': 'My Page Title'}  # 数据字典
+    # return render(request, '/templates/trend.html', context)  # 使用模板
+    return render(request, 'UnderWater/UnderWater_user.html', context)
