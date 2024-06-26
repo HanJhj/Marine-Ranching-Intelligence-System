73075018ebac1bb4f18493d728989f3e27e88732
Yuzhao Peng <3143935826@qq.com>
2024-06-26 21:59:54 +0800

    新增鱼群增长模拟

--- mytry/UnderWater/views.py
+++ mytry/UnderWater/views.py
@@ -32,6 +32,9 @@ def UnderWater(request):
         water = Water.objects.get(time__startswith=today)
     except Water.DoesNotExist:
         water = Water.objects.get(time__startswith='2024-06-22')
+    # 鱼的种类数
+    species_counts = Fish.objects.values('Species').distinct().count()
+
     context = {'all_fish': all_fish,
                'fishnum':fishnum,
                'fish_counts':fish_counts,
@@ -43,13 +46,54 @@ def UnderWater(request):
                 'Perch_weights': list(perch_weights),
                 'Pike_weights': list(pike_weights),
                 'Smelt_weights': list(smelt_weights),
-                'water':water
+                'water': water,
+                'species_counts': species_counts
                }
     return render(request, 'UnderWater/UnderWater.html', context)
 
 def UnderWater_user(request):
-    context = {'title': 'My Page Title'}  # 数据字典
+    # context = {'title': 'My Page Title'}  # 数据字典
     # return render(request, '/templates/trend.html', context)  # 使用模板
+    all_fish = Fish.objects.all()
+    # 鱼群总量
+    fishnum=Fish.objects.all().count()
+    # 每一种鱼群的数量
+    fish_counts=Fish.objects.values('Species').annotate(count=Count('Species'))
+        # 将查询结果转换为ECharts所需的数据格式
+    chart_data = [{"name": species['Species'], "value": species['count']} for species in fish_counts]
+    # 用于画鱼群分布曲线
+    bream_weights = Fish.objects.filter(Species='Bream').values_list('Weight', flat=True)
+    roach_weights = Fish.objects.filter(Species='Roach').values_list('Weight', flat=True)
+    whitefish_weights = Fish.objects.filter(Species='Whitefish').values_list('Weight', flat=True)
+    parkki_weights = Fish.objects.filter(Species='Parkki').values_list('Weight', flat=True)
+    perch_weights = Fish.objects.filter(Species='Perch').values_list('Weight', flat=True)
+    pike_weights = Fish.objects.filter(Species='Pike').values_list('Weight', flat=True)
+    smelt_weights = Fish.objects.filter(Species='Smelt').values_list('Weight', flat=True)
+
+    # water
+    try:
+        # 获取今天的数据
+        today = pd.Timestamp.now().strftime('%Y-%m-%d')
+        water = Water.objects.get(time__startswith=today)
+    except Water.DoesNotExist:
+        water = Water.objects.get(time__startswith='2024-06-22')
+    # 鱼的种类数
+    species_counts = Fish.objects.values('Species').distinct().count()
+    
+    context = {'all_fish': all_fish,
+               'fishnum':fishnum,
+               'fish_counts':fish_counts,
+               "chart_data": chart_data,
+                'Bream_weights': list(bream_weights),
+                'Roach_weights': list(roach_weights),
+                'Whitefish_weights': list(whitefish_weights),
+                'Parkki_weights': list(parkki_weights),
+                'Perch_weights': list(perch_weights),
+                'Pike_weights': list(pike_weights),
+                'Smelt_weights': list(smelt_weights),
+                'water': water,
+                'species_counts': species_counts
+               }
     return render(request, 'UnderWater/UnderWater_user.html', context)
 
 def generate_csv(request):
